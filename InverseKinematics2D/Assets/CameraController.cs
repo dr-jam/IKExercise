2683a76f84ee5a62b25817c6c42b191c766f3d73
asandhuzero <asandhuzero@gmail>
2020-01-30 13:25:29 -0800

    Correct IK2D assignment

---
+++ InverseKinematics2D/Assets/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera camera;
+    public Transform target;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                target.position = hit.point;
+            }
+        }
+    }
+}
